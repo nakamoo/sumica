3752100fee9ae982ab56ee24887fc311386d1e76
sean <seann999@gmail.com>
2017-08-06 23:47:27 +0900

    適当に移動

--- launch.sh
+++
@@ -1,4 +0,0 @@
-(cd hai_server; node server.js) &
-(cd nn_server; python detect_server.py) &
-(cd image_handler; python imagereceiver.py) &
-(cd client; python client.py http://localhost:5000/upload)

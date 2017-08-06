a10aa731e3deb67a0099be97fb921fc83794004d
sean <seann999@gmail.com>
2017-08-06 15:28:43 +0900

    minor

--- recognition_server/detect_server.py
+++ recognition_server/detect_server.py
@@ -18,6 +18,7 @@ from io import BytesIO
 import cv2
 import tracker
 import colorsys
+import random
 
 if len(sys.argv) == 2 and sys.argv[1] == "ssd":
     import ssd as nn
@@ -42,8 +43,8 @@ def process_image_dataurl():
 
     return dets
 
-def detect():
-    image = np.array(Image.open("image.png"))
+def detect(image):
+    #image = np.array(Image.open("image.png"))
     
     if len(image.shape) == 2:
         image = np.expand_dims(image, 2)
@@ -68,7 +69,7 @@ def visualize(frame, all_boxes, win_name="frame"):
         cv2.putText(frame, name, (det[0], det[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, c, 2)
         cv2.rectangle(frame, (det[0], det[1]), (int(det[2]), int(det[3])), c, 2)
 
-    #cv2.imshow(win_name, frame)
+    #cv2.imshow(win_name, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
     #cv2.waitKey(1)
 
 clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
@@ -78,9 +79,10 @@ def preprocess(img):
     img_yuv[:,:,0] = clahe.apply(img_yuv[:,:,0])
     img_output = cv2.cvtColor(img_yuv, cv2.COLOR_YUV2BGR)
 
-    img_output = cv2.fastNlMeansDenoisingColored(img_output,None,5,5,7,21)
+    #img_output = cv2.fastNlMeansDenoisingColored(img_output,None,5,5,7,21)
 
-    cv2.imwrite('image.png', img_output)
+    #cv2.imwrite('image.png', img_output)
+    return img_output
 
 track = tracker.Tracker()
 
@@ -93,22 +95,29 @@ track = tracker.Tracker()
 def process_image():
     f = request.files["image"]
 
-    f.save("image.png")
+    fname = "/tmp/{}.png".format(random.randint(0,10000000))
 
-    preprocess(cv2.imread('image.png'))
+    f.save(fname)
+
+    imgmat = cv2.cvtColor(cv2.imread(fname), cv2.COLOR_BGR2RGB)#preprocess(cv2.imread(fname))
 
     print("detecting...")
-    dets = detect()
+    dets = detect(imgmat)
     print("detected")
 
-    visualize(cv2.imread('image.png'), dets)
+    visualize(imgmat, dets)
 
-    clean_dets = track.update(dets)
+    os.remove(fname)
 
-    visualize(cv2.imread('image.png'), clean_dets, "clean")
+    #clean_dets = track.update(dets)
+
+    #visualize(cv2.imread('image.png'), clean_dets, "clean")
 
     return json.dumps(dets)
 
 if __name__ == "__main__":
     #context = (cer, key)
-    app.run(host='0.0.0.0', threaded=False, use_reloader=False, debug=True, port=5002)
+    try:
+        app.run(host='0.0.0.0', threaded=False, use_reloader=False, debug=True, port=5002)
+    except KeyboardInterrupt:
+        print("closing")

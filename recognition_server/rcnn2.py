33a0ef95c3899bfd65fb8bfc05f523cb12502d27
sean <seann999@gmail.com>
2017-08-10 01:42:27 +0900

    rcnn

--- recognition_server/rcnn2.py
+++ recognition_server/rcnn2.py
@@ -75,24 +75,26 @@ IMAGE_SIZE = (12, 8)
 sess = tf.Session(graph=detection_graph)
 
 def detect(image):
-    #for image_path in ["room.jpg"]:
-      image = Image.open(image_path)
-      # the array based representation of the image will be used later in order to prepare the
-      # result image with boxes and labels on it.
-      image_np = load_image_into_numpy_array(image)
-      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-      image_np_expanded = np.expand_dims(image_np, axis=0)
-      image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
-      # Each box represents a part of the image where a particular object was detected.
-      boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-      # Each score represent how level of confidence for each of the objects.
-      # Score is shown on the result image, together with the class label.
-      scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
-      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
-      # Actual detection.
-      (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
-          feed_dict={image_tensor: image_np_expanded})
+  # the array based representation of the image will be used later in order to prepare the
+  # result image with boxes and labels on it.
+  image_np = load_image_into_numpy_array(image)
+  # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+  image_np_expanded = np.expand_dims(image_np, axis=0)
+  image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+  # Each box represents a part of the image where a particular object was detected.
+  boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+  # Each score represent how level of confidence for each of the objects.
+  # Score is shown on the result image, together with the class label.
+  scores = detection_graph.get_tensor_by_name('detection_scores:0')
+  classes = detection_graph.get_tensor_by_name('detection_classes:0')
+  num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+  # Actual detection.
+  (boxes, scores, classes, num_detections) = sess.run(
+      [boxes, scores, classes, num_detections],
+      feed_dict={image_tensor: image_np_expanded})
 
-      print(num_detections)
+  print(boxes.shape)
+  print(scores.shape)
+  print(classes.shape)
+
+  

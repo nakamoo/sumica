f1623bba7544ff88c5b5ff47b8a8da23f80fbae5
nakamoo <k00ook17@gmail.com>
2017-08-09 22:25:38 +0900

    Add http parameter

--- new_server/hai/hai.py
+++ new_server/hai/hai.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
 
 import uuid
@@ -11,9 +11,9 @@ def home_page():
     return render_template('index.html')
 
 
-@app.route('/data/image')
+@app.route('/data/images')
 def get_image_data():
-    return render_template('index.html')
+    return "Not implemented", 404
 
 
 @app.route('/data/images', methods=['POST'])
@@ -24,46 +24,49 @@ def post_image_data():
     data['filename'] = filename
     mongo.db.images.insert_one(data)
 
-    return render_template('index.html')
+    data.pop("_id")
+    return jsonify(data), 201
 
 
 @app.route('/data/hue')
 def get_hue_data():
-    return render_template('index.html')
+    return "Not implemented", 404
 
 
 @app.route('/data/hue', methods=['POST'])
 def post_hue_data():
     data = request.form.to_dict()
     mongo.db.hue.insert_one(data)
-    return render_template('index.html')
+    data.pop("_id")
+    return jsonify(data), 201
 
 
 @app.route('/data/youtube')
 def get_youtube_data():
-    return render_template('index.html')
+    return "Not implemented", 404
 
 
 @app.route('/data/youtube', methods=['POST'])
 def post_youtube_data():
     data = request.form.to_dict()
     mongo.db.youtube.insert_one(data)
-    return render_template('index.html')
+    data.pop("_id")
+    return jsonify(data), 201
 
 
 @app.route('/controllers')
 def get_controllers():
-    return render_template('index.html')
+    return "Not implemented", 404
 
 
 @app.route('/controllers/update', methods=['POST'])
 def update_controllers():
-    return render_template('index.html')
+    return "Not implemented", 404
 
 
 @app.route('/controllers/execute', methods=['POST'])
 def execute_controllers():
-    return render_template('index.html')
+    return "Not implemented", 404
 
 if __name__ == '__main__':
     app.run()

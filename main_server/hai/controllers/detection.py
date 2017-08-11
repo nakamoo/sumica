d8fb0d4a25f515ce3a52fe61c64a1479b9ea9375
sean <seann999@gmail.com>
2017-08-11 16:15:11 +0900

    trigger events, file moving

---
+++ main_server/hai/controllers/detection.py
@@ -0,0 +1,15 @@
+from controller import Controller
+import numpy as np
+
+class Detection(Controller):
+    def __init__(self):
+    	pass
+
+    def on_event(self, event, data):
+    	if event == "image":
+    		print("image received")
+    		print(data)
+
+    def execute(self):
+        return "turn off"
+

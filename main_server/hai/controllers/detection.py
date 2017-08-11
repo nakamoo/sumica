4322cf3ee02ffe45531c7271400c7c5983e9d18c
sean <seann999@gmail.com>
2017-08-11 16:58:18 +0900

    fucking tabs

--- main_server/hai/controllers/detection.py
+++ main_server/hai/controllers/detection.py
@@ -3,18 +3,18 @@ import numpy as np
 
 class Detection(Controller):
     def __init__(self):
-    	pass
+        pass
 
     def on_event(self, event, data):
-    	if event == "image":
-    		print("image received")
-    		print(data)
+        if event == "image":
+            print("image received")
+            print(data)
 
-    		addr = "http://localhost:5002/detect"
+            addr = "http://localhost:5002/detect"
 
-    		r = requests.post(addr, files={'image': open("images/" + data["filename"], "rb")}, data=data)
+            r = requests.post(addr, files={'image': open("images/" + data["filename"], "rb")}, data=data)
 
-        	print("detections: {}".format(r.text))
+            print("detections: {}".format(r.text))
 
     def execute(self):
         return "turn off"

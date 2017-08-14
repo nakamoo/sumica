8faffba2cbbe75a158f82181647e561174076658
sean <seann999@gmail.com>
2017-08-14 23:33:22 +0900

    save settings

---
+++ main_server/hai/controllers/settings.py
@@ -0,0 +1,33 @@
+from .controller import Controller
+import database as db
+import os
+
+class Settings(Controller):
+    def __init__(self, user):
+        n = db.mongo.settings.find_one({"user_name": user})
+
+        if not n:
+            db.mongo.settings.insert_one({"user_name": user, "save_images": True})
+
+        self.load_settings()
+
+    def load_settings(self):
+        n = db.mongo.settings.find_one({"user_name": user})
+        self.save_images = n["save_images"]
+
+    def on_event(self, event, data):
+        if event == "chat":
+            if data["message"]["text"] == "save images on":
+                self.save_images = True
+            elif data["message"]["text"] == "save images off":
+                self.save_images = False
+
+            db.mongo.settings.update_one({"user_name": self.user_name}, {"save_images": self.save_images})
+        elif event == "image":
+            if not self.save_images:
+                for n in db.mongo.images.find({"user_name": self.user_name}):
+                    print("removing {}".format(n["filename"]))
+                    os.remove("./images/" + n["filename"])
+
+    def execute(self):
+        return []
--- main_server/hai/database.py
+++ main_server/hai/database.py
@@ -8,6 +8,7 @@ import importlib
 from controllers.detection import Detection
 from controllers.chatbot import Chatbot
 from controllers.hello import HelloController
+from controllers.settings import Settings
 
 def load_controller_modules():
     fs = ['controllers.{}'.format(f[:-3]) for f in os.listdir('controllers') if f.endswith('.py')]
@@ -26,7 +27,7 @@ def load_controller_modules():
 control_mods = load_controller_modules()
 
 def standard_controllers(user_name):
-    return [Detection(), Chatbot(user_name), HelloController(user_name)]
+    return [Detection(), Chatbot(user_name), HelloController(user_name), Settings(user_name)]
 
 # TODO: use DB
 controllers_objects = {}

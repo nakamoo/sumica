56febeb4fc2cb8e263037c218fc5a2069ed0a45c
sean <seann999@gmail.com>
2017-08-16 02:54:18 +0900

    async pose

--- main_server/hai/controllers/pose.py
+++ main_server/hai/controllers/pose.py
@@ -1,13 +1,25 @@
 from .controller import Controller
 import subprocess
 from shutil import copyfile
+import threading
 
 def subprocess_cmd(command):
     process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
     proc_stdout = process.communicate()[0].strip()
     print proc_stdout
 
+def update_loop():
+    while True:
+        subprocess_cmd('cd ~/openpose; ./openpose/build/examples/openpose/openpose.bin --no_display --image_dir ~/HAI/main_server/hai/pose_tmp --write_images outs --num_gpu 2 --num_gpu_start 2')
+        for f in glob.glob("./pose_tmp/*"):
+            os.remove(f)
+
 class Pose(Controller):
+    print("starting pose estimation thread...")
+    thread_stream = threading.Thread(target=update_loop)
+    thread_stream.daemon = True
+    thread_stream.start()
+
     def __init__(self):
         pass
 
@@ -15,9 +27,6 @@ class Pose(Controller):
         if event == "image":
             image_path = './images/' + data['filename']
             copyfile(image_path, './pose_tmp/' + data['filename'])
-            subprocess_cmd('cd ~/openpose; ./openpose/build/examples/openpose/openpose.bin --no_display --image_dir ~/HAI/main_server/hai/pose_tmp --write_images outs --num_gpu 2 --num_gpu_start 2')
-            for f in glob.glob("./pose_tmp/*"):
-                os.remove(f)
 
     def execute(self):
         return []

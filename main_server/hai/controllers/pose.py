2f9dbed54aebbe14d52cb95997e5c05a5684f8fb
nakamoo <k00ook17@gmail.com>
2017-09-20 19:30:45 +0900

    Indent

--- main_server/hai/controllers/pose.py
+++ main_server/hai/controllers/pose.py
@@ -12,33 +12,36 @@ import database as db
 for f in glob.glob("./pose_data/*"):
     os.remove(f)
 
+
 def subprocess_cmd(command):
     proc = subprocess.Popen([command],
-                        stdout=subprocess.PIPE,
-                        stderr=subprocess.PIPE, shell=True)
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE, shell=True)
     stdout, stderr = proc.communicate()
-    #out = check_output([command])
-    #proc_stdout = process.communicate()
+    # out = check_output([command])
+    # proc_stdout = process.communicate()
     print(stdout, stderr)
+
 
 def manage_data():
     json_files = glob.glob("./pose_data/*")
     for f in json_files:
-                try:
-                  #print(open(f, "r").readlinesi())
-                  pts = json.load(open(f, "r"))
-                  name = f[:-15].split("/")[-1] + ".png"
-                  pose_data = {"keypoints": pts}
-                  image_info = db.mongo.images.find_one({"filename": name})
-                  pose_data.update(image_info)
-                  #db.mongo.pose.insert_one(pose_data)
+        try:
+            # print(open(f, "r").readlinesi())
+            pts = json.load(open(f, "r"))
+            name = f[:-15].split("/")[-1] + ".png"
+            pose_data = {"keypoints": pts}
+            image_info = db.mongo.images.find_one({"filename": name})
+            pose_data.update(image_info)
+            # db.mongo.pose.insert_one(pose_data)
 
-                  db.mongo.images.update_one({"filename": name}, {'$set': {'keypoints': pts}}, upsert=False)
-                  os.remove(f)
-                except Exception as e:
-                  #pass
-                  print("missing file", f)
-    #time.sleep(0.1)
+            db.mongo.images.update_one({"filename": name}, {'$set': {'keypoints': pts}}, upsert=False)
+            os.remove(f)
+        except Exception as e:
+            # pass
+            print("missing file", f)
+            # time.sleep(0.1)
+
 
 def update_loop():
     while True:
@@ -52,17 +55,18 @@ def update_loop():
                 os.remove(f)
          """
         manage_data()
-        #else:
+        # else:
         #    time.sleep(1)
 
-#if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
-#print("starting pose estimation thread...")
-#thread_stream = threading.Thread(target=update_loop)
-#thread_stream.daemon = True
-#thread_stream.start()
 
-#print("starting pose estimation subprocess...")
-#subprocess.call(['cd ~/openpose; ./build/examples/user_code/mytest.bin --image_dir ~/HAI/main_server/hai/pose_tmp --write_keypoint_json ~/HAI/main_server/hai/pose_data --num_gpu 2 --num_gpu_start 1 --face --hand'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+# if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+# print("starting pose estimation thread...")
+# thread_stream = threading.Thread(target=update_loop)
+# thread_stream.daemon = True
+# thread_stream.start()
+
+# print("starting pose estimation subprocess...")
+# subprocess.call(['cd ~/openpose; ./build/examples/user_code/mytest.bin --image_dir ~/HAI/main_server/hai/pose_tmp --write_keypoint_json ~/HAI/main_server/hai/pose_data --num_gpu 2 --num_gpu_start 1 --face --hand'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 
 class Pose(Controller):
     def __init__(self):
@@ -77,7 +81,7 @@ class Pose(Controller):
                 image_path = app.config['RAW_IMG_DIR'] + data['filename']
             print("copying to pose_tmp")
             copyfile(image_path, './pose_tmp/' + data['filename'])
-        elif event == "timer": 
+        elif event == "timer":
             manage_data()
 
     def execute(self):

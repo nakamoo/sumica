ac55da3a1d715414b1cf51e23dc6287786820840
nakamoo <k00ook17@gmail.com>
2017-10-01 19:06:57 +0900

    Add irkit

--- client/utils/actions.py
+++ client/utils/actions.py
@@ -1,25 +1,28 @@
 from subprocess import Popen
 import subprocess
 import json
+from irkit import IrkitInternetAP
+
 
 def act_list(actions):
-	for action in actions:
-		if "platform" in action and "data" in action:
-			act(action["platform"], action["data"])
+    for action in actions:
+        if "platform" in action and "data" in action:
+            act(action["platform"], action["data"])
+
 
 def act(platform, data):
-	if platform == "youtube":
-		print("OPENING YOUTUBE")
-		Popen("node utils/youtube.js {}".format(data), shell=True)
-	elif platform == "shell":
-		Popen("{}".format(data), shell=True)
-	elif platform == "print":
-		print(data)
-	elif platform == "sound":
-		Popen("play ../sounds/{}".format(data), shell=True)
-	elif platform == "hue":
-		with open('utils/hue_state.json', 'w+') as outfile:
-		    json.dump(json.loads(data), outfile)
+    if platform == "youtube":
+        print("OPENING YOUTUBE")
+        Popen("node utils/youtube.js {}".format(data), shell=True)
+    elif platform == "shell":
+        Popen("{}".format(data), shell=True)
+    elif platform == "print":
+        print(data)
+    elif platform == "sound":
+        Popen("play ../sounds/{}".format(data), shell=True)
+    elif platform == "hue":
+        with open('utils/hue_state.json', 'w+') as outfile:
+            json.dump(json.loads(data), outfile)
 
-		out = subprocess.check_output(['node', 'utils/hue.js', 'set_state'])
-		print(out.decode('utf-8'))
+        out = subprocess.check_output(['node', 'utils/hue.js', 'set_state'])
+        print(out.decode('utf-8'))
---
+++ client/utils/irkit.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import requests
+import json
+import configparser
+
+
+class IrkitInternetAPI:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('./irkit.ini')
+        self.endpoint = "http://" + self.config['DEFAULT']['ip']
+
+    def set_param(self):
+        print("TVリモコンをIRKitに向けて，on/offボダンを押して下さい")
+        input("準備ができたらどれかキーを押して下さい")
+        param_tv = self.get_messages()
+        self.config['TV']['param'] == param_tv
+
+        print("エアコンのリモコンをIRKitに向けて，on/offボダンを押して下さい")
+        input("準備ができたらどれかキーを押して下さい")
+        param_ac = self.get_messages()
+        self.config['AirConditioning']['param'] == param_ac
+
+        with open('irkit.ini', 'w') as configfile:
+            self.config.write(configfile)
+
+        print("OK")
+
+    def get_messages(self):
+        headers = {'X-Requested-With': "curl"}
+        url = self.endpoint + "/messages"
+
+        r = requests.get(url, headers=headers)
+        if (r.text == '') or (r.status_code != 200):
+            print("Error. Please retry.")
+
+        return r.text
+
+    def post_messages(self, device):
+        message = self.config[device]['param']
+        message = json.dumps(message)
+
+        params = {'message': message}
+
+        url = self.endpoint + "/messages"
+        headers = {'X-Requested-With': "curl"}
+        r = requests.post(url, headers=headers, params=params)
+
+
+if __name__ == '__main__':
+    IrkitInternetAPI().set_param()

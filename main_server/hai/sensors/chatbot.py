59dcb23364e894c81fd2c3112b9a6d881e8678aa
nakamoo <k00ook17@gmail.com>
2017-10-23 14:26:44 +0900

    Work for me

--- main_server/hai/sensors/chatbot.py
+++ main_server/hai/sensors/chatbot.py
@@ -8,7 +8,7 @@ bp = Blueprint("chatbot", __name__)
 # temporary
 fb2user = {}
 
-bot_id = "318910425200757"
+bot_id = "157695071486517"
 VERIFY_TOKEN = 'my_voice_is_my_password_verify_me'
 
 """
@@ -31,39 +31,42 @@ def post_fb_data():
     return "ok", 201
 """
 
+
 @bp.route('/data/fb', methods=['GET'])
 def handle_verification():
-  print("Handling Verification.")
-  if request.args.get('hub.verify_token', '') == VERIFY_TOKEN:
-    print("Verification successful!")
-    return request.args.get('hub.challenge', '')
-  else:
-    print("Verification failed!")
-    return 'Error, wrong validation token'
+    print("Handling Verification.")
+    if request.args.get('hub.verify_token', '') == VERIFY_TOKEN:
+        print("Verification successful!")
+        return request.args.get('hub.challenge', '')
+    else:
+        print("Verification failed!")
+        return 'Error, wrong validation token'
+
 
 @bp.route('/data/fb', methods=['POST'])
 def handle_messages():
-  payload = request.get_json()
+    payload = request.get_json()
 
-  for event in messaging_events(payload):
-    fb_id = event["sender"]["id"]
-    
-    username = None
+    for event in messaging_events(payload):
+        fb_id = event["sender"]["id"]
 
-    n = db.mongo.fb_users.find_one({"fb_id": fb_id})
-    if n:
-        username = n["id"]
-    
-    if fb_id != bot_id:
-        db.trigger_controllers(username, "chat", event)
-        
-  return "ok"
+        username = None
+
+        n = db.mongo.fb_users.find_one({"fb_id": fb_id})
+        if n:
+            username = n["id"]
+
+        if fb_id != bot_id:
+            db.trigger_controllers(username, "chat", event)
+
+    return "ok"
+
 
 def messaging_events(payload):
-  #Generate tuples of (sender_id, message_text) from the
-  #provided payload.
-  
-  messaging_events = payload["entry"][0]["messaging"]
-  for event in messaging_events:
-    if "message" in event:
-        yield event
+    # Generate tuples of (sender_id, message_text) from the
+    # provided payload.
+
+    messaging_events = payload["entry"][0]["messaging"]
+    for event in messaging_events:
+        if "message" in event:
+            yield event

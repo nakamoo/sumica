b502d240a48d57cd616ad377c734758358993291
nakamoo <k00ook17@gmail.com>
2017-11-18 03:59:56 +0900

    multiple music

--- client/managers/youtube.py
+++ client/managers/youtube.py
@@ -16,7 +16,10 @@ class Manager:
                 try:
                     Popen('pkill -9 mpv', shell=True)
                     time.sleep(0.3)
-                    Popen("mpv 'https://www.youtube.com/watch?v=3jWRrafhO7M' --no-video > /dev/null 2>&1", shell=True)
+                    if act['data'] == 'j':
+                        Popen("mpv 'https://www.youtube.com/watch?v=3jWRrafhO7M' --no-video > /dev/null 2>&1", shell=True)
+                    elif act['data'] == 'n':
+                        Popen("mpv 'https://www.youtube.com/watch?v=WrO9PTpuSSs' --no-video > /dev/null 2>&1", shell=True)
                     self.now_playing = act['data']
                 except:
                     traceback.print_exc()
@@ -30,5 +33,5 @@ class Manager:
 
 if __name__ == "__main__":
     youtubeplayer = Manager('sample', '1.0.0.0', None)
-    youtubeplayer.execute([{'platform': 'play_youtube', 'data': 'nujabes'}])
+    youtubeplayer.execute([{'platform': 'play_youtube', 'data': 'n'}])
     # youtubeplayer.execute([{'platform': 'stop_youtube', 'data': ''}])

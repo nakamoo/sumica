ae7089cef5c6cc4b9d548a910df4d3711ecd4e77
nakamoo <k00ook17@gmail.com>
2017-12-17 15:52:06 +0900

    Commit

--- main_server/hai/controllers/irkit.py
+++ main_server/hai/controllers/irkit.py
@@ -1,19 +1,40 @@
 import time
 from .controller import Controller
 from database import mongo
+from server_actors import chatbot
 
 
 class IRKit(Controller):
     def __init__(self, user):
         self.re = None
         self.user = user
+        self.tv_on = False
+        n = mongo.fb_users.find_one({"id": user})
+        if n:
+            self.fb_id = n["fb_id"]
 
     def on_event(self, event, data):
         if event == "chat":
             msg = data["message"]["text"].split()
             if msg[0] == "irkit":
-                if msg[1] == "TV" or msg[1] == "AirConditioning":
-                    self.re = [{"platform": "irkit", "data": msg[1:]}]
+                if msg[1] == "TV":
+                    if msg[2] == "on":
+                        if self.tv_on:
+                            chatbot.send_fb_message(self.fb_id, "TVは既についています")
+                        else:
+                            chatbot.send_fb_message(self.fb_id, "TVをつけます")
+                            self.re = [{"platform": "irkit", "data": msg[1:]}]
+                            self.tv_on = True
+                    elif msg[2] == "off":
+                        if self.tv_on:
+                            chatbot.send_fb_message(self.fb_id, "TVをけします")
+                            self.re = [{"platform": "irkit", "data": msg[1:]}]
+                            self.tv_on = False
+                        else:
+                            chatbot.send_fb_message(self.fb_id, "TVは既にきえています")
+
+                elif msg[1] == "AirConditioning":
+                    pass
 
                 inserted_data = dict()
                 inserted_data['message'] = msg

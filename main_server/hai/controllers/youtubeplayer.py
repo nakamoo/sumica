3d890ef27202a1bed32c8df8999f8affad709b28
nakamoo <k00ook17@gmail.com>
2018-01-05 13:31:17 +0900

    Add trivial change'

--- main_server/hai/controllers/youtubeplayer.py
+++ main_server/hai/controllers/youtubeplayer.py
@@ -13,7 +13,8 @@ class YoutubePlayer(Controller):
             msg = data["message"]["text"].split()
             if msg[0] == "music":
                 if msg[1] == "play":
-                    self.re = [{"platform": "play_youtube", "data": msg[2]}]
+                    self.re = [{"platform": "play_youtube", "data": msg[2],
+                                "confirmation": msg[2] + "を再生しますか?"}]
                 if msg[1] == "stop":
                     self.re = [{"platform": "stop_youtube", "data": ""}]
 
@@ -29,7 +30,7 @@ class YoutubePlayer(Controller):
                 keyword = msg[:msg.find('を流')]
                 self.re.append({"platform": "tts", "data": keyword+"を検索して再生します"})
                 self.re.append({"platform": "play_youtube", "data": keyword})
-            if "音楽を消して" in msg:
+            if "音楽を消し" in msg or '音楽を止めt ':
                 self.re.append({"platform": "tts", "data": "音楽を消します"})
                 self.re.append({"platform": "stop_youtube", "data": ''})
 

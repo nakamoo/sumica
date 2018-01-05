5fd51de59ec77f65588f42d067e7e2b810bc0270
nakamoo <k00ook17@gmail.com>
2018-01-06 03:04:19 +0900

    utils/speechrecognition.py

--- client/utils/speechrecognition.py
+++ client/utils/speechrecognition.py
@@ -1,25 +1,46 @@
+import sys
+sys.path.insert(0, "./")
+
+import time
 import speech_recognition as sr 
+import utils.tts as tts
 
-def confirm():
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        audio = r.listen(source)
-    try:
-        text = r.recognize_google(audio, language="ja")
-        print(text)
-    except sr.UnknownValueError:
-        print("Sphinx could not understand audio")
-        return None
-    except sr.RequestError as e:
-        print("Sphinx error; {0}".format(e))
-        return None
+from managers.hotword import snowboydecoder
 
-    if text == "はい":
-        return True
-    elif text == "いいえ":
-        return False
+def listen_confirmation():
+    confirmation_answer = None
+    start_time = time.time()
+    detected = False
+    models = ["hotwords/yes.pmdl", "hotwords/no.pmdl"]
+    detector = snowboydecoder.HotwordDetector(models, sensitivity=[0.5, 0.5], audio_gain=1)
+    def interrupt_callback():
+        nonlocal start_time, detected
+        if detected:
+            return True
+        if time.time() - start_time > 5:
+            return True
+        else:
+            return False
+    def detected_callback(data, ans):
+        nonlocal confirmation_answer, detected
+        if ans == 1:
+            confirmation_answer = True
+            detected = True
+        elif ans == 2:
+            confirmation_answer = False
+            detected = True
 
-    return None
+    detector.start(detected_callback=detected_callback, interrupt_check=interrupt_callback)
+    return confirmation_answer
+
+def confirm(confirmation):
+    tts.say(confirmation)
+    time.sleep(0.5)
+    ans = listen_confirmation()
+    if ans is None:
+        tts.say("上手く聞こえませんでした,もう一度お願いします")
+        ans = listen_confirmation()
+    return ans
 
 if __name__ == "__main__":
-    confirm()
+    print(confirm('確認です'))

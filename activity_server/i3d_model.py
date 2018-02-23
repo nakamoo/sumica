21672d376f5d1a390c3de32ca610cd76fa7d2c5a
sean <seann999@gmail.com>
2018-02-24 08:04:17 +0900

    http request

--- activity_server/i3d_model.py
+++ activity_server/i3d_model.py
@@ -1,6 +1,6 @@
 import sys
 
-i3d_root = '../kinetics_i3d_pytorch'
+i3d_root = '../../kinetics_i3d_pytorch'
 sys.path.insert(0, i3d_root)
 
 import numpy as np
@@ -9,6 +9,7 @@ from src.i3dpt import I3D
 
 rgb_weights_path = i3d_root + '/model/model_rgb.pth'
 classes_path = i3d_root + '/data/kinetic-samples/label_map.txt'
+gpu = 0
 
 class I3DModel:
     def __init__(self):
@@ -17,12 +18,12 @@ class I3DModel:
         i3d_rgb = I3D(num_classes=400, modality='rgb')
         i3d_rgb.eval()
         i3d_rgb.load_state_dict(torch.load(rgb_weights_path))
-        i3d_rgb.cuda(2)
+        i3d_rgb.cuda(gpu)
 
         self.i3d = i3d_rgb
         
     def predict(self, sample):
-        sample_var = torch.autograd.Variable(torch.from_numpy(sample).float().cuda(2))
+        sample_var = torch.autograd.Variable(torch.from_numpy(sample).float().cuda(gpu))
         out_var, out_logit = self.i3d(sample_var)
         out_tensor = out_var.data.cpu()
 
